c1c577b35a0a93157d5b0bc3a65b427d50c5791a
Esophose <esophose@gmail.com>
2021-07-20 19:57:53 -0600

    Added Ender Dragon attack phase condition

--- src/main/java/dev/rosewood/roseloot/loot/condition/tags/entity/EnderDragonConditions.java
+++ src/main/java/dev/rosewood/roseloot/loot/condition/tags/entity/EnderDragonConditions.java
@@ -1,11 +1,50 @@
 package dev.rosewood.roseloot.loot.condition.tags.entity;
 
+import dev.rosewood.roseloot.loot.LootContext;
 import dev.rosewood.roseloot.loot.condition.EntityConditions;
+import dev.rosewood.roseloot.loot.condition.LootCondition;
+import dev.rosewood.roseloot.loot.condition.LootConditions;
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.entity.LivingEntity;
 
 public class EnderDragonConditions extends EntityConditions {
 
     public EnderDragonConditions() {
-        // Nothing to see here
+        LootConditions.registerTag("ender-dragon-phase", EnderDragonPhaseCondition.class);
+    }
+
+    public static class EnderDragonPhaseCondition extends LootCondition {
+
+        private List<EnderDragon.Phase> phases;
+
+        public EnderDragonPhaseCondition(String tag) {
+            super(tag);
+        }
+
+        @Override
+        public boolean checkInternal(LootContext context) {
+            LivingEntity looted = context.getLootedEntity();
+            if (!(looted instanceof EnderDragon))
+                return false;
+            return this.phases.contains(((EnderDragon) looted).getPhase());
+        }
+
+        @Override
+        public boolean parseValues(String[] values) {
+            this.phases = new ArrayList<>();
+
+            for (String value : values) {
+                try {
+                    EnderDragon.Phase phase = EnderDragon.Phase.valueOf(value.toUpperCase());
+                    this.phases.add(phase);
+                } catch (Exception ignored) { }
+            }
+
+            return !this.phases.isEmpty();
+        }
+
     }
 
 }
