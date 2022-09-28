ef43f87e25a073d9ddd4f92ccf781de2f1218d96
Esophose <esophose@gmail.com>
2022-09-28 15:49:57 -0600

    Use new RoseStacker API

--- src/main/java/dev/rosewood/roseloot/hook/RoseStackerHook.java
+++ src/main/java/dev/rosewood/roseloot/hook/RoseStackerHook.java
@@ -1,12 +1,9 @@
 package dev.rosewood.roseloot.hook;
 
 import dev.rosewood.rosestacker.api.RoseStackerAPI;
-import dev.rosewood.rosestacker.manager.ConfigurationManager;
 import dev.rosewood.rosestacker.stack.StackedEntity;
-import dev.rosewood.rosestacker.utils.PersistentDataUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.event.entity.EntityDamageEvent;
 
 public class RoseStackerHook {
 
@@ -30,13 +27,7 @@ public class RoseStackerHook {
         if (stackedEntity == null || stackedEntity.getStackSize() <= 1)
             return false;
 
-        // This whole section was copy/pasted from RoseStacker
-        // This should be changed to use API in the future, but it just doesn't exist right now
-        EntityDamageEvent lastDamageCause = entity.getLastDamageCause();
-        return stackedEntity.getStackSettings().shouldKillEntireStackOnDeath()
-                || (ConfigurationManager.Setting.SPAWNER_DISABLE_MOB_AI_OPTIONS_KILL_ENTIRE_STACK_ON_DEATH.getBoolean() && PersistentDataUtils.isAiDisabled(entity))
-                || (lastDamageCause != null && ConfigurationManager.Setting.ENTITY_KILL_ENTIRE_STACK_CONDITIONS.getStringList().stream().anyMatch(x -> x.equalsIgnoreCase(lastDamageCause.getCause().name())))
-                || (entity.getKiller() != null && entity.getKiller().hasPermission("rosestacker.killentirestack"));
+        return stackedEntity.isEntireStackKilledOnDeath();
     }
 
 }
