30dcfb183290452c78530a3d730921dc3b2ed945
Esophose <esophose@gmail.com>
2022-11-24 12:28:51 -0700

    Added an option to broadcast messages to all players

--- src/main/java/dev/rosewood/roseloot/loot/item/MessageLootItem.java
+++ src/main/java/dev/rosewood/roseloot/loot/item/MessageLootItem.java
@@ -2,10 +2,11 @@ package dev.rosewood.roseloot.loot.item;
 
 import dev.rosewood.roseloot.loot.context.LootContext;
 import dev.rosewood.roseloot.util.NumberProvider;
-import java.util.Optional;
+import java.util.function.Consumer;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
@@ -16,28 +17,34 @@ public class MessageLootItem implements TriggerableLootItem {
     private final MessageType messageType;
     private final String message;
     private final NumberProvider fadeIn, duration, fadeOut;
+    private final boolean broadcast;
 
-    public MessageLootItem(MessageType messageType, String message, NumberProvider fadeIn, NumberProvider duration, NumberProvider fadeOut) {
+    public MessageLootItem(MessageType messageType, String message, NumberProvider fadeIn, NumberProvider duration, NumberProvider fadeOut, boolean broadcast) {
         this.messageType = messageType;
         this.message = message;
         this.fadeIn = fadeIn;
         this.duration = duration;
         this.fadeOut = fadeOut;
+        this.broadcast = broadcast;
     }
 
     @Override
     public void trigger(LootContext context, Location location) {
-        Optional<Player> lootingPlayer = context.getLootingPlayer();
-        if (lootingPlayer.isEmpty())
-            return;
+        Consumer<Player> consumer = player -> {
+            switch (this.messageType) {
+                case CHAT_RAW -> player.spigot().sendMessage(ChatMessageType.CHAT, ComponentSerializer.parse(context.applyPlaceholders(this.message)));
+                case CHAT -> player.sendMessage(context.formatText(this.message));
+                case HOTBAR -> player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(context.formatText(this.message)));
+                case TITLE -> player.sendTitle(context.formatText(this.message), null, this.fadeIn.getInteger(), this.duration.getInteger(), this.fadeOut.getInteger());
+                case SUBTITLE -> player.sendTitle(null, context.formatText(this.message), this.fadeIn.getInteger(), this.duration.getInteger(), this.fadeOut.getInteger());
+            }
+        };
 
-        Player player = lootingPlayer.get();
-        switch (this.messageType) {
-            case CHAT_RAW -> player.spigot().sendMessage(ChatMessageType.CHAT, ComponentSerializer.parse(context.applyPlaceholders(this.message)));
-            case CHAT -> player.sendMessage(context.formatText(this.message));
-            case HOTBAR -> player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(context.formatText(this.message)));
-            case TITLE -> player.sendTitle(context.formatText(this.message), null, this.fadeIn.getInteger(), this.duration.getInteger(), this.fadeOut.getInteger());
-            case SUBTITLE -> player.sendTitle(null, context.formatText(this.message), this.fadeIn.getInteger(), this.duration.getInteger(), this.fadeOut.getInteger());
+        if (this.broadcast) {
+            for (Player player : Bukkit.getOnlinePlayers())
+                consumer.accept(player);
+        } else {
+            context.getLootingPlayer().ifPresent(consumer);
         }
     }
 
@@ -50,8 +57,9 @@ public class MessageLootItem implements TriggerableLootItem {
         NumberProvider fadeIn = NumberProvider.fromSection(section, "fade-in", 20);
         NumberProvider duration = NumberProvider.fromSection(section, "duration", 20);
         NumberProvider fadeOut = NumberProvider.fromSection(section, "fade-out", 20);
+        boolean broadcast = section.getBoolean("broadcast", false);
 
-        return new MessageLootItem(messageType, message, fadeIn, duration, fadeOut);
+        return new MessageLootItem(messageType, message, fadeIn, duration, fadeOut, broadcast);
     }
 
     public enum MessageType {
