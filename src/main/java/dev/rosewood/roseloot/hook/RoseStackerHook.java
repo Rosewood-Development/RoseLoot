2b04687f165eff0f88b6c59e0c0c3a8e077377a5
Esophose <esophose@gmail.com>
2024-03-30 17:17:43 -0600

    Increase the specificity of the RoseStacker hook for multikills

--- src/main/java/dev/rosewood/roseloot/hook/RoseStackerHook.java
+++ src/main/java/dev/rosewood/roseloot/hook/RoseStackerHook.java
@@ -4,7 +4,10 @@ import dev.rosewood.rosestacker.api.RoseStackerAPI;
 import dev.rosewood.rosestacker.manager.ConfigurationManager;
 import dev.rosewood.rosestacker.stack.StackedEntity;
 import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 
 public class RoseStackerHook {
 
@@ -22,10 +25,36 @@ public class RoseStackerHook {
 
         RoseStackerAPI api = RoseStackerAPI.getInstance();
         StackedEntity stackedEntity = api.getStackedEntity(entity);
-        return api.isEntityStackMultipleDeathEventCalled()
-                && stackedEntity != null
-                && stackedEntity.getStackSize() != 1
-                && (stackedEntity.isEntireStackKilledOnDeath() || ConfigurationManager.Setting.ENTITY_MULTIKILL_ENABLED.getBoolean());
+
+        // Don't ignore if single entity kill or not a stack
+        if (!api.isEntityStackMultipleDeathEventCalled()
+                || stackedEntity == null
+                || stackedEntity.getStackSize() == 1)
+            return false;
+
+        // Ignore if entire stack kill
+        if (stackedEntity.isEntireStackKilledOnDeath())
+            return true;
+
+        // Is partial stack kill
+        Player killer = entity.getKiller();
+        if (!ConfigurationManager.Setting.ENTITY_MULTIKILL_ENABLED.getBoolean())
+            return false;
+
+        if (ConfigurationManager.Setting.ENTITY_MULTIKILL_PLAYER_ONLY.getBoolean() && killer == null)
+            return false;
+
+        if (!ConfigurationManager.Setting.ENTITY_MULTIKILL_ENCHANTMENT_ENABLED.getBoolean())
+            return true;
+
+        if (killer == null)
+            return false;
+
+        Enchantment requiredEnchantment = Enchantment.getByKey(NamespacedKey.fromString(ConfigurationManager.Setting.ENTITY_MULTIKILL_ENCHANTMENT_TYPE.getString()));
+        if (requiredEnchantment == null)
+            return false;
+
+        return killer.getInventory().getItemInMainHand().getEnchantmentLevel(requiredEnchantment) > 0;
     }
 
 }
