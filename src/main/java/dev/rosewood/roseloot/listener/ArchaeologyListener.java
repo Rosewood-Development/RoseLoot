15ce61e28a57611a52815f0b20b738850fae837e
Esophose <esophose@gmail.com>
2024-10-08 20:08:40 -0600

    Fix overwrite-existing behavior for ARCHAEOLOGY tables

--- src/main/java/dev/rosewood/roseloot/listener/ArchaeologyListener.java
+++ src/main/java/dev/rosewood/roseloot/listener/ArchaeologyListener.java
@@ -1,5 +1,6 @@
 package dev.rosewood.roseloot.listener;
 
+import com.google.common.collect.Iterables;
 import dev.rosewood.rosegarden.RosePlugin;
 import dev.rosewood.rosegarden.utils.EntitySpawnUtil;
 import dev.rosewood.roseloot.config.SettingKey;
@@ -13,6 +14,7 @@ import dev.rosewood.roseloot.loot.table.LootTableTypes;
 import dev.rosewood.roseloot.manager.LootTableManager;
 import dev.rosewood.roseloot.util.LootUtils;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -60,10 +62,11 @@ public class ArchaeologyListener extends LazyLootTableListener {
                 .put(LootContextParams.LOOTER, player);
 
         LootTable lootTable = brushableBlock.getLootTable();
-        if (lootTable != null) {
+        boolean hasExistingLootTable = lootTable != null;
+        if (hasExistingLootTable) {
             lootContextBuilder.put(LootContextParams.VANILLA_LOOT_TABLE_KEY, lootTable.getKey());
         } else if (brushableBlock.getItem() != null && brushableBlock.getItem().getType() != Material.AIR) {
-            return; // already generated
+            return; // already generated, your loot table must add an item to the brushable block otherwise it will keep generating more roseloot
         }
 
         LootContext lootContext = lootContextBuilder.build();
@@ -76,7 +79,17 @@ public class ArchaeologyListener extends LazyLootTableListener {
             } else {
                 brushableBlock.setItem(items.remove(0));
             }
-        } else if (!items.isEmpty() && (brushableBlock.getItem() == null || brushableBlock.getItem().getType() == Material.AIR)) {
+        } else if (hasExistingLootTable) {
+            // Unfortunately for us the loot table hasn't been run yet, so we have to run it ourselves, hopefully
+            // this doesn't introduce any compatibility issues
+            org.bukkit.loot.LootContext bukkitLootContext = new org.bukkit.loot.LootContext.Builder(block.getLocation()).build();
+            Collection<ItemStack> vanillaItems = brushableBlock.getLootTable().populateLoot(LootUtils.RANDOM, bukkitLootContext);
+            if (!vanillaItems.isEmpty()) {
+                brushableBlock.setItem(Iterables.get(vanillaItems, 0));
+            } else if (!items.isEmpty()) {
+                brushableBlock.setItem(items.remove(0));
+            }
+        } else if (!items.isEmpty()) {
             brushableBlock.setItem(items.remove(0));
         }
 
