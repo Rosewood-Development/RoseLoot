0a858ff83983a76d843d5a11da68a70329154729
jhqwqmc <jhcraft@qq.com>
2026-05-17 22:57:11 +0800

    Compatible with the latest version of CraftEngine

--- Plugin/src/main/java/dev/rosewood/roseloot/hook/items/CraftEngineItemProvider.java
+++ Plugin/src/main/java/dev/rosewood/roseloot/hook/items/CraftEngineItemProvider.java
@@ -2,7 +2,7 @@ package dev.rosewood.roseloot.hook.items;
 
 import dev.rosewood.roseloot.loot.context.LootContext;
 import net.momirealms.craftengine.bukkit.api.CraftEngineItems;
-import net.momirealms.craftengine.core.item.CustomItem;
+import net.momirealms.craftengine.bukkit.item.BukkitItemDefinition;
 import net.momirealms.craftengine.core.util.Key;
 import org.bukkit.inventory.ItemStack;
 
@@ -15,8 +15,8 @@ public class CraftEngineItemProvider extends ItemProvider {
     @Override
     public ItemStack getItem(LootContext context, String id) {
         if (!this.isEnabled()) return null;
-        CustomItem<ItemStack> itemStackCustomItem = CraftEngineItems.byId(Key.of(id));
-        return itemStackCustomItem == null ? null : itemStackCustomItem.buildItemStack();
+        BukkitItemDefinition itemStackCustomItem = CraftEngineItems.byId(id);
+        return itemStackCustomItem == null ? null : itemStackCustomItem.buildBukkitItem();
     }
 
     @Override
